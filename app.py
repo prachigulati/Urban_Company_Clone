04357e51f13c76c144138ac3656625e5da51496e
prachigulati <prachigulati569@gmail.com>
2025-02-18 10:27:52 +0530

    temporary session

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request  # type: ignore
+from flask import Flask, render_template, request, flash , redirect, url_for, session # type: ignore
 # from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.secret_key= "xyz"
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 # db= SQLAlchemy(app)
 
@@ -17,18 +18,35 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register', methods=['GET','POST'])
 def register():
     if request.method=="POST":
         pass
     return render_template('register.html')
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     if request.method=="POST":
-        pass
-    return render_template('login.html')
+        user=request.form["nm"]
+        session["user"]=user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template('login.html')
+    
+@app.route("/user")
+def user():
+    if "user" in session:
+        user=session["user"]
+        return f"<h1>{user} , you are logged in!</h1>"
+    else:
+        return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
